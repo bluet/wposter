60c8e337ab3b6e58e2e6aee29458b32a6fc59c5c
OliWave <a0924100192@gmail.com>
2018-05-20 17:17:49 +0800

    get the file of .cvs

--- renderer.js
+++ renderer.js
@@ -1,9 +1,24 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var inputElement = document.getElementById("customFile");
-inputElement.addEventListener("change", handleFiles, false);
-function handleFiles() {
-  var fileList = this.files; /* now you can work with the file list */
-  console.log(fileList);
+
+function myfile() {
+	var x = document.getElementById('customFile');
+	var txt = '';
+	if ('files' in x) {
+		if (x.files.length != 0) {
+			txt += "<br><strong>" + 1 + ". file</strong><br>";
+			var file = x.files[0];
+            if ('name' in file) {
+                txt += "name: " + file.name + "<br>";
+            }
+            if ('size' in file) {
+                txt += "size: " + file.size + " bytes <br>";
+            }
+		}
+	}
+
+	document.getElementById("demo").innerHTML = txt;
+	console.log(x);
+	
 }
