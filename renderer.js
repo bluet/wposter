4b34aeaa0a51c5552486dc8844c7e0a27fa43e3c
OliWave <a0924100192@gmail.com>
2018-05-20 12:16:20 +0800

    remove modules, add input file label in index.html and get file instance at renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,9 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var inputElement = document.getElementById("customFile");
+inputElement.addEventListener("change", handleFiles, false);
+function handleFiles() {
+  var fileList = this.files; /* now you can work with the file list */
+  console.log(fileList);
+}
