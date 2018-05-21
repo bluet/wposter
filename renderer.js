0ec2852eba75e4acf431779867823ea5a35cfab6
OliWave <a0924100192@gmail.com>
2018-05-21 23:37:44 +0800

    get the full path of csv, convert to json fomat and analyze json successfully.

--- renderer.js
+++ renderer.js
@@ -1,7 +1,6 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
-var csv = require("csvtojson");
 
 function myfile(File) {
 
@@ -22,69 +21,51 @@ function myfile(File) {
 		}
 	}
 
-	
-
 	document.getElementById("demo").innerHTML = txt;
 
+	// 'date tag as YYYYMM' : is time 
+	// 'field6'             : is image
+	// 'field5'             : is title
 	
-	// csv().fromFile(x.files[0].path).on("end_parsed", function(jsonArrayObj) {
-	// 	console.log(jsonArrayObj);
-  	// })
+	// 'field9'             : is context 1
+	// 'field12'            : is context 2
+	// 'field4'             : is context 3
+	
+	// 'field7'             : is tag 1
+	// 'field8'             : is tag 2
+	// 'field10'            : is tag 3 
+	// 'field11'            : is tag 4
 
+	var format_data = [];
+	
+	var csv = require("csvtojson");
+	csv().fromFile(x.files[0].path).on("end_parsed", function(jsonArrayObj) {
+		console.log(jsonArrayObj);
+  		
+  		for(let i in jsonArrayObj){
 
+			let time = jsonArrayObj[i]['date tag as YYYYMM'];
+			let image = jsonArrayObj[i]['field6'];
+			let title = jsonArrayObj[i]['field12'];
 
-}
-
-// 'date tag as YYYYMM' : is time 
-// 'field6'             : is image
-// 'field5'             : is title
-
-// 'field9'             : is context 1
-// 'field12'            : is context 2
-// 'field4'             : is context 3
-
-// 'field7'             : is tag 1
-// 'field8'             : is tag 2
-// 'field10'            : is tag 3 
-// 'field11'            : is tag 4
-
-
-var tmp_path = '/Users/yiyuchang/Desktop/dev/EDQblogbot - 工作表1.csv';
-var format_data = [];
-csv().fromFile(tmp_path).on("end_parsed", function(jsonArrayObj) {
-	// console.log(jsonArrayObj[0]['date tag as YYYYMM']);
-
-	// console.log(jsonArrayObj)
-
-	for(let i in jsonArrayObj){
-
-		let time    = jsonArrayObj[i]['date tag as YYYYMM'];
-		let image    = jsonArrayObj[i]['field6'];
-		let title   = jsonArrayObj[i]['field12'];
-
-		let context = jsonArrayObj[i]['field9'] + '\n' +
+			let context = jsonArrayObj[i]['field9'] + '\n' +
 					  jsonArrayObj[i]['field12'] + '\n' +
 					  jsonArrayObj[i]['field4'];
 		
-		let tag     = [ jsonArrayObj[i]['field7'],
-						jsonArrayObj[i]['field8'],
-						jsonArrayObj[i]['field9'],
-						jsonArrayObj[i]['field10']]
+			let tag = [jsonArrayObj[i]['field7'],
+					jsonArrayObj[i]['field8'],
+					jsonArrayObj[i]['field9'],
+					jsonArrayObj[i]['field10']]
 		
-		format_data.push({
-			"time" : time,
-			"image" : image,
-			"title" : title,
-			"context" : context,
-			"tag" : tag
-		})
-		// console.log(context);
-	}
-
-	// console.log(jsonArrayObj)
-})
-
-console.log(format_data)
-
-
-// exports.format_data = format_data;
+			format_data.push({
+				"time" : time,
+				"image" : image,
+				"title" : title,
+				"context" : context,
+				"tag" : tag
+			})
+			console.log(context);
+		}
+		console.log(format_data);
+  	})
+}
