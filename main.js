cbf038b853e432039e3f90fde4613ed223a96083
OliWave <a0924100192@gmail.com>
2018-05-22 12:18:18 +0800

    add ejs (not complete)

--- main.js
+++ main.js
@@ -7,31 +7,48 @@ const url = require('url')
 
 let mainWindow
 
+const ejs = require('ejs');
+
+let data = {user: {name: "Jeff"}};
+let options = {root: __dirname};
+
 function createWindow () {
 
     mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    mainWindow.loadURL(url.format({
+      pathname: path.join(__dirname, 'index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
 
-  mainWindow.on('closed', function () {
-    mainWindow = null
-  })
+    ejs.renderFile('index.ejs', data, options, function (err, str) {
+        if (err) {
+          console.log(err);
+        }
+
+        // Load the rendered HTML to the BrowserWindow.
+        mainWindow.loadURL('data:text/html;charset=utf-8,' + encodeURI(str));
+    });
+    
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
+
+
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
-})
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})  
 
 app.on('activate', function () {
-  if (mainWindow === null) {
-    createWindow()
-  }
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
+  
--- renderer.js
+++ renderer.js
@@ -106,5 +106,4 @@ function postToWP(){
 		}
 	}
 
-
 }
