4a0f879578330d221fce6fcdddad881015f7121c
aFish <tako0903@icloud.com>
2018-05-22 19:36:50 +0800

    div size

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let mainWindow
 
 function createWindow () {
 
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 700})
 
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
