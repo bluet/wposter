394c68f885758b838d15f817896f5dfe6c6e7865
BlueT - Matthew - 練喆明 <BlueT@BlueT.org>
2018-05-28 02:54:06 +0800

    thumbnail works (and in two ways)

--- testdata/wp.js
+++ testdata/wp.js
@@ -1,100 +1,83 @@
-// s105213007@ncnu.edu.tw
+var fs = require('fs');
 var wordpress = require( "wordpress" );
 var client = wordpress.createClient({
     url: "https://ppsn8787.wordpress.com/",
     username: "ppsn8787",
     password: "ilovesn87"
 });
-
-// client.getPosts(function( error, posts ) {
-//     console.log( "Found " + posts.length + " posts!" );
-// });
-
-// client.listMethods(function(n,data){
-//     console.log(data['wp.newPost']);
-// })
+// s105213007@ncnu.edu.tw
 
 
-var fs = require('fs');
-// var file = fs.readFileSync( "test.png" );
-// client.authenticatedCall( "wp.uploadFile", {
-//     name: "test.png",
-//     type: "image/png",
-//     bits: file
-// }, function() {
-//     console.log( arguments );
-// });
+// create, upload, edit
+client.newPost(
+    {
+        title: "newPost, upload pic, and editPost",
+        content: 'Contents without daIMG',
+        status: "publish",
+        termNames: {
+            "category": ["Javascript", "Node"],
+            "post_tag": ["api", "fun", "js"]
+        }
+    },
+    function (error , postid) {
+        console.log("Return from newPost:");
+        console.log(arguments);
 
-// var read_file = fs.readFile("test.png",{ encoding: "base64" });
+        var file = fs.readFileSync( "test.png" );
+        client.authenticatedCall(
+            "wp.uploadFile",
+            {
+                name: "test.png",
+                type: "image/png",
+                bits: file,
+                postId: postid,
+            },
+            function (error, file_info) {
+                console.log("Return from uploadFile:");
+                console.dir(arguments);
 
-// // console.log(read_file)
-
-
-// upload file to media library
-// var file = fs.readFileSync( "test.png" );
-// client.uploadFile({
-//     name: 'file-001',
-//     type: 'image/png',
-//     bits: read_file,
-//     postId: 20
-// },function(error,file){
-//     console.log(error);
-//     console.log(file);
-// });
-
-
-
-client.newPost({
-    title: "gg",
-    content: 'test context',
-    status: "publish",
-    termNames: {
-		"category": ["Javascript", "Node"],
-		"post_tag": ["api", "fun", "js"]
+                client.editPost(
+                    postid,
+                    { thumbnail: file_info.id },
+                    function ( error ) {
+                        console.log("Return from editPost:");
+                        console.dir(arguments);
+                    }
+                );
+            }
+        );
     }
-},function( error , id){
-    // console.log(error);
-    console.log(id);
-    var file = fs.readFileSync( "test.png" );
-    client.authenticatedCall( "wp.uploadFile", {
-        name: "test.png",
+);
+
+
+// just upload and create
+var file = fs.readFileSync( "boom-lines.png" );
+client.authenticatedCall(
+    "wp.uploadFile",
+    {
+        name: "boom-lines.png",
         type: "image/png",
         bits: file,
-        postId: id,
-    },function( error, file ) {
-        console.dir(arguments)
-        console.log( file );
-        client.editPost(id,{thumbnail:
-            {thumbnail:245 }
-        },function( error ){
-            console.log(error);
-        })
-    });
-});
+    },
+    function (error, file_info) {
+        console.log("Return from uploadFile:");
+        console.dir(arguments);
 
-// client.editPost(posts[0].id, {thumbnail : { thumbnail : "http://xxxxxxxxxxxxx.org/wp-content/uploads/2017/07/japanese-rice-bowl-pottery-150x150.jpg" } }, function( error ) {})
-
-
-
-// // turn around XD
-// var file = fs.readFileSync( "test.png" );
-// client.authenticatedCall( "wp.uploadFile", {
-//     name: "test.png",
-//     type: "image/png",
-//     bits: file,
-//     postId: id
-// }, function() {
-//     client.newPost({
-//         title: "My First Post",
-//         content: 'test context',
-//         status: "publish",
-//         thumbnail: file,
-//         termNames: {
-//             "category": ["Javascript", "Node"],
-//             "post_tag": ["api", "fun", "js"]
-//         }
-//     },function( error, id ){
-//         console.log(error);
-//         console.log(id);
-//     });
-// });
+        client.newPost(
+            {
+                title: "Just uploadFile and newPost",
+                content: 'Contents without daIMG',
+                status: "publish",
+                thumbnail: file_info.id,
+                termNames: {
+                    "category": ["Javascript", "Node"],
+                    "post_tag": ["api", "fun", "js"]
+                }
+            },
+            function ( error ) {
+                console.log("Return from newPost:");
+                console.dir(arguments);
+            }
+        );
+    }
+);
